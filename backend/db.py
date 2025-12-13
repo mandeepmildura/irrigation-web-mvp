a7c76563c42a29345a333a597f27688c9226e9a5
mandeepmildura <86057570+mandeepmildura@users.noreply.github.com>
2025-12-13 23:34:42 +1100

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DB_URL = "sqlite:///./irrigation.db"
+DB_URL = "sqlite:////var/data/irrigation.db"
+
 
 engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
